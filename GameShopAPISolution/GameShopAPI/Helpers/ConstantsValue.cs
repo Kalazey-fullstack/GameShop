48d00ffe4582892a5694ae5180cd97218f8a718e
Kalazey-fullstack <kala.drakkae@gmail.com>
2025-05-12 16:53:12 +0200

    :hammer: add user controller file into the feature/backend/user-controller

---
+++ GameShopAPISolution/GameShopAPI/Helpers/ConstantsValue.cs
@@ -0,0 +1,8 @@
+﻿namespace GameShopAPI.Helpers
+{
+    public class ConstantsValue
+    {
+        public const string UserAdminRole = "Admin";
+        public const string userRole = "User";
+    }
+}
