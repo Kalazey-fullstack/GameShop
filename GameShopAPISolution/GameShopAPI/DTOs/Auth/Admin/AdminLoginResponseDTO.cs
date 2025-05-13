6843a3db00fb267bc81f11ea2cf8e5bb131b6391
Kalazey <maillot.mathieu62@gmail.com>
2025-05-13 10:33:29 +0200

    :hammer: Add Admin parts of the Authentification

---
+++ GameShopAPISolution/GameShopAPI/DTOs/Auth/Admin/AdminLoginResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿using GameShopAPI.Models.Users;
+
+namespace GameShopAPI.DTOs.Auth.Admin
+{
+    public class AdminLoginResponseDTO
+    {
+        public bool IsSuccessful { get; set; }
+        public string? ErrorMessage { get; set; }
+        public AdminUser? AdminUser { get; set; }
+        public string? Token { get; set; }
+    }
+}
---
+++ GameShopAPISolution/GameShopAPI/DTOs/Auth/Admin/AdminRegisterResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using GameShopAPI.Models.Users;
+
+namespace GameShopAPI.DTOs.Auth.Admin
+{
+    public class AdminRegisterResponseDTO
+    {
+        public bool IsSuccessful { get; set; }
+        public string? ErrorMessage {  get; set; }
+        public AdminUser? AdminUser { get; set; }
+    }
+}
